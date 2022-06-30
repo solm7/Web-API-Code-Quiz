b1a977b41d1d16529e56a168e13497043ad2dee7
Roronoa <dup1040@gmail.com>
2022-06-29 21:53:55 -0400

    Third Commit

--- Assets/script.js
+++ Assets/script.js
@@ -8,15 +8,32 @@ function start() {
 }
 
 function nextQuestion() {
+    var questions = [{
+        Question: "Which is a HTML element?",
+        Answer1: "Rock",
+        Answer2: "Paper",
+        Answer3: "Scissors",
+        Answer4: "Div",
+    }, {
+        Question: "Which is a Coding Language?",
+        Answer1: "Basketball",
+        Answer2: "Soccer",
+        Answer3: "CSS",
+        Answer4: "Football",
+    },
+    {
+        Question: "Which language is Dynamic?",
+        Answer1: "Spanish",
+        Answer2: "Portuguese",
+        Answer3: "JavaScript",
+        Answer4: "HTML",
+    }];
 
 }
 
 
 function selectAnswer() {
+    var button = document.querySelector(".button")
+    button.items(3)
 
 }
-
-var startButton = document.querySelector("#start-button")
-console.log(startButton)
-
-// startButton.addEventListener("click", start())
