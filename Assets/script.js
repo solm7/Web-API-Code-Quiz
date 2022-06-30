1512849b74bdc87c0bcf549aa1749ed08e0e7a06
Roronoa <dup1040@gmail.com>
2022-06-29 23:08:11 -0400

    Fourth Commit

--- Assets/script.js
+++ Assets/script.js
@@ -4,36 +4,71 @@ function start() {
     show.className = "grid"
     var startButton = document.querySelector("#start-button")
     startButton.className = "hide"
+    var nextButton = document.querySelector("#next-button")
+    nextButton.className = "grid"
 
 }
 
 function nextQuestion() {
-    var questions = [{
-        Question: "Which is a HTML element?",
-        Answer1: "Rock",
-        Answer2: "Paper",
-        Answer3: "Scissors",
-        Answer4: "Div",
-    }, {
-        Question: "Which is a Coding Language?",
-        Answer1: "Basketball",
-        Answer2: "Soccer",
-        Answer3: "CSS",
-        Answer4: "Football",
-    },
-    {
-        Question: "Which language is Dynamic?",
-        Answer1: "Spanish",
-        Answer2: "Portuguese",
-        Answer3: "JavaScript",
-        Answer4: "HTML",
-    }];
+
+    if (count < questions.length) {
+
+        var question = questions[count]
+        var questionTag = document.querySelector("#question")
+        questionTag.innerHTML = question.Question
+        var buttonTags = document.querySelector("#answer-buttons").children
+        console.log(buttonTags)
+        var answer1 = buttonTags[0]
+        var answer2 = buttonTags[1]
+        var answer3 = buttonTags[2]
+        var answer4 = buttonTags[3]
+        answer1.innerHTML = question.Answer1
+        answer2.innerHTML = question.Answer2
+        answer3.innerHTML = question.Answer3
+        answer4.innerHTML = question.Answer4
+        console.log(count)
+        count++
+
+    }
+
 
 }
+var questions = [{
+    Question: "Which is a Coding Language?",
+    Answer1: "Basketball",
+    Answer2: "Soccer",
+    Answer3: "CSS",
+    Answer4: "Football",
+
+    correctAnswer: "CSS"
+},
+{
+    Question: "Which language is Dynamic?",
+    Answer1: "Spanish",
+    Answer2: "Portuguese",
+    Answer3: "JavaScript",
+    Answer4: "HTML",
+
+    correctAnswer: "JavaScript"
+}];
+var count = 0
+var nextButton = document.querySelector("#next-button")
+nextButton.addEventListener("click", nextQuestion)
 
 
-function selectAnswer() {
-    var button = document.querySelector(".button")
-    button.items(3)
+function selectAnswer(element) {
+    element.innerHTML
 
 }
+var buttonTags = document.querySelector("#answer-buttons").children
+console.log(buttonTags)
+var answer1 = buttonTags[0]
+var answer2 = buttonTags[1]
+var answer3 = buttonTags[2]
+var answer4 = buttonTags[3]
+
+answer1.addEventListener("click", selectAnswer)
+answer2.addEventListener("click", selectAnswer)
+answer3.addEventListener("click", selectAnswer)
+answer4.addEventListener("click", selectAnswer)
+
